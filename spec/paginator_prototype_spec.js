84272391fe6951da9ceb4f4f8e61295b724034c7
Tyler Mercier <tylermercier@gmail.com>
2012-03-11 12:59:51 -0600

    move v1.1.html, updated index. Wrote a single failing test

--- spec/paginator_prototype_spec.js
+++ spec/paginator_prototype_spec.js
@@ -1,59 +1,13 @@
 describe("Paginator", function() {
   var paginator;
-  var items;
 
   beforeEach(function() {
-    items = [1, 2, 3, 4, 5]
-    paginator = new Pagintor();
-    song = new Song();
+    paginator = {};
   });
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-  });
-
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrow("song is already playing");
+  describe("when time in available", function() {
+    it("should write tests here", function() {
+      expect(false).not.toBe(false);
     });
   });
 });
